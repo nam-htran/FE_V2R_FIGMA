15d777a65493f101660e156e28cf85702d5b43a0
HoangNam <hnamt04@gmail.com>
2025-10-08 21:45:46 +0700

    final v3

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import ImageGallery from "@/components/ImageGallery";
 import WorkflowCTA from "@/components/WorkflowCTA";
 import Header from "@/components/Header";
+import Pricing from "@/components/Pricing";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <Hero />
       <ImageGallery />
+      <Pricing />
       <WorkflowCTA />
       <Footer />
     </>
